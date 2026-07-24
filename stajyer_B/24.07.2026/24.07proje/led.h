1242004854484c7522328bb07d154d353ccdeb4e
burakucar55 <burak55ucar55@gmail.com>
2026-07-24 16:31:32 +0300

    Delete stajyer_B/24.07.2026/24.07proje directory

--- stajyer_B/24.07.2026/24.07proje/led.h
+++
@@ -1,9 +0,0 @@
-#ifndef LED_H
-#define LED_H
-
-void Led_Init(void);
-void Led_SelfTest(void); // Açılışta for döngüsü ile test
-void Led_ChaseStep(void); // Bir sonraki LED'e geçiş
-void Led_AllOff(void); // Tüm LED'leri kapat (Pause modu için)
-
-#endif /* LED_H */
